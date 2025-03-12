ae0cff861031b48cca2f96ac266e142c5cefa2ba
Muhammad Saqib Ali <m.saqib.a7865@gmail.com>
2025-03-12 20:48:03 +0500

    Successfully created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import "./App.css";
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </>
   );
