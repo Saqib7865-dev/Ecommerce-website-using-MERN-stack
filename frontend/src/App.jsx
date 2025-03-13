919e8610c2dd3c402b2fb8b77bd8929d6f310e48
Muhammad Saqib Ali <m.saqib.a7865@gmail.com>
2025-03-13 23:39:51 +0500

    Completed the design of Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   );
 }
