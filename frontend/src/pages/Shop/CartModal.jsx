a620aa8446829f1c843a367d7fae8f20bc3b1dc5
Muhammad Saqib Ali <m.saqib.a7865@gmail.com>
2025-03-18 22:57:56 +0500

    Fixed bg overlay effect

--- frontend/src/pages/Shop/CartModal.jsx
+++ frontend/src/pages/Shop/CartModal.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const CartModal = ({ products, isCartOpen, onClose }) => {
   return (
     <div
-      className={`fixed z-[1000] inset-0 bg-black bg-opacity-50 transition-opacity  text-white ${
-        isCartOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+      className={`fixed z-[1000] inset-0 bg-black transition-opacity  text-white ${
+        isCartOpen ? "opacity-50" : "opacity-0 pointer-events-none"
       }`}
       style={{ transition: "opacity 300ms" }}
     >
