176b2d6a93d56f7d761892d341def4ee6e1d635f
Muhammad Saqib Ali <m.saqib.a7865@gmail.com>
2025-03-23 02:19:04 +0500

    Done with cart features

--- frontend/src/pages/Shop/CartModal.jsx
+++ frontend/src/pages/Shop/CartModal.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-
+import OrderSummary from "./OrderSummary";
+import { useDispatch } from "react-redux";
+import {
+  clearCart,
+  removeFromCart,
+  updateQuantity,
+} from "../../redux/features/cart/cartSlice";
 const CartModal = ({ products, isCartOpen, onClose }) => {
+  const dispatch = useDispatch();
+  const handleQuantity = (type, id) => {
+    const payload = { type, id };
+    dispatch(updateQuantity(payload));
+  };
+  const handleRemove = (e, id) => {
+    e.preventDefault();
+    dispatch(removeFromCart({ id }));
+  };
+  const handleClearCart = (e) => {
+    e.preventDefault();
+    dispatch(clearCart());
+  };
   return (
     <div
-      className={`fixed z-[1000] inset-0 bg-black bg-opacity-90 transition-opacity ${
-        isCartOpen ? "opacity-80" : "opacity-0 pointer-events-none"
+      className={`fixed z-[1000] inset-0 bg-black/80 transition-opacity ${
+        isCartOpen ? "opacity-100" : "opacity-0 pointer-events-none"
       }`}
-      style={{ transition: "opacity 300ms" }}
     >
       <div
         className={`fixed right-0 top-0 md:w-1/3 w-full bg-white h-full overflow-y-auto transition-transform ${
@@ -17,7 +35,7 @@ const CartModal = ({ products, isCartOpen, onClose }) => {
         }}
       >
         <div className="!p-4 !mt-4">
-          <div className="flex justify-between items-center mb-4">
+          <div className="flex justify-between items-center !mb-4">
             <h4 className="text-xl font-semibold">Your Cart</h4>
             <button
               className="text-gray-600 hover:text-gray-900"
@@ -26,7 +44,7 @@ const CartModal = ({ products, isCartOpen, onClose }) => {
               <i className="ri-xrp-fill bg-black p-1 text-white cursor-pointer"></i>
             </button>
           </div>
-          <div>
+          <div className="cart-items">
             {products.length === 0 ? (
               <div>Your cart is empty</div>
             ) : (
@@ -55,17 +73,30 @@ const CartModal = ({ products, isCartOpen, onClose }) => {
                         </p>
                       </div>
                       <div className="flex md:justify-start !justify-end items-center !mt-2">
-                        <button className="size-6 flex items-center justify-center !px-1.5 !rounded-full bg-gray-200 text-gray-700 hover:bg-primary hover:text-white !ml-8">
+                        <button
+                          className="size-6 flex items-center justify-center !px-1.5 !rounded-full bg-gray-200 text-gray-700 hover:bg-primary hover:text-white !ml-8"
+                          onClick={() =>
+                            handleQuantity("decrement", product.id)
+                          }
+                        >
                           -
                         </button>
                         <span className="!px-2 text-center !mx-1">
                           {product.quantity}
                         </span>
-                        <button className="size-6 flex items-center justify-center !px-1.5 !rounded-full bg-gray-200 text-gray-700 hover:bg-primary hover:text-white">
+                        <button
+                          className="size-6 flex items-center justify-center !px-1.5 !rounded-full bg-gray-200 text-gray-700 hover:bg-primary hover:text-white"
+                          onClick={() =>
+                            handleQuantity("increment", product.id)
+                          }
+                        >
                           +
                         </button>
                         <div className="!ml-5">
-                          <button className="text-red-500 hover:text-red-800 !mr-4 cursor-pointer">
+                          <button
+                            className="text-red-500 hover:text-red-800 !mr-4 cursor-pointer"
+                            onClick={(e) => handleRemove(e, product.id)}
+                          >
                             Remove
                           </button>
                         </div>
@@ -76,6 +107,9 @@ const CartModal = ({ products, isCartOpen, onClose }) => {
               })
             )}
           </div>
+          {products.length > 0 && (
+            <OrderSummary handleClearCart={handleClearCart} />
+          )}
         </div>
       </div>
     </div>
--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -25,19 +25,52 @@ export const cartSlice = createSlice({
       state.tax = setTax(state);
       state.grandTotal = setGrandTotal(state);
     },
+    updateQuantity: (state, action) => {
+      const products = state.products.map((product) => {
+        if (product.id === action.payload.id) {
+          if (action.payload.type === "increment") {
+            product.quantity += 1;
+          } else if (action.payload.type === "decrement") {
+            product.quantity =
+              product.quantity - 1 < 1 ? 1 : product.quantity - 1;
+          }
+        }
+        return product;
+      });
+      state.selectedItems = setSelectedItems(state);
+      state.totalPrice = setTotalPrice(state);
+      state.tax = setTax(state);
+      state.grandTotal = setGrandTotal(state);
+    },
+    removeFromCart: (state, action) => {
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload.id
+      );
+      state.selectedItems = setSelectedItems(state);
+      state.totalPrice = setTotalPrice(state);
+      state.tax = setTax(state);
+      state.grandTotal = setGrandTotal(state);
+    },
+    clearCart: (state) => {
+      state.products = [];
+      state.selectedItems = 0;
+      state.totalPrice = 0;
+      state.tax = 0;
+      state.grandTotal = 0;
+    },
   },
 });
 // utility Function
 export const setSelectedItems = (state) => {
-  state.products.reduce((total, product) => {
+  return state.products.reduce((total, product) => {
     return Number(total + product.quantity);
-  });
+  }, 0);
 };
 
 export const setTotalPrice = (state) => {
-  state.products.reduce((total, product) => {
+  return state.products.reduce((total, product) => {
     return Number(total + product.price * product.quantity);
-  });
+  }, 0);
 };
 
 export const setTax = (state) => {
@@ -47,5 +80,6 @@ export const setTax = (state) => {
 export const setGrandTotal = (state) => {
   return setTotalPrice(state) + setTax(state);
 };
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, updateQuantity, removeFromCart, clearCart } =
+  cartSlice.actions;
 export default cartSlice.reducer;
