02b3f82cb5e57d71bae9ef80138819b8467d5869
Muhammad Saqib Ali <m.saqib.a7865@gmail.com>
2025-03-25 10:26:48 +0500

    Removed node_modules directories

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 // middleware
 app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ extended: true, limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Routes
